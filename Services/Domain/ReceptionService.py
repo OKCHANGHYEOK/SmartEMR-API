fffbca2b1eb7970b1f9c95ec5022f3c7caf64973
OK CHANG HYEOK <dkdldy2@naver.com>
2026-06-23 09:24:29 +0900

    fix : 접수 관련 동작 수정

--- Services/Domain/ReceptionService.py
+++ Services/Domain/ReceptionService.py
@@ -27,6 +27,7 @@ class ReceptionService(BaseService):
 
         item.RCP_Status = request.RCP_Status
         item.RCP_Route = request.RCP_Route
+        item.RCP_YYMMDD = request.RCP_YYMMDD
 
         item.Keyword = request.Keyword
         item.PageSize = request.PageSize
@@ -66,15 +67,11 @@ class ReceptionService(BaseService):
         item.PAT_Sex = retPAT.PAT_Sex
         item.PAT_Age = retPAT.PAT_Age
 
+        item.RCP_Idx = request.RCP_Idx
         item.RCP_Status = request.RCP_Status
         item.RCP_Route = request.RCP_Route
         item.RCP_VisitType = request.RCP_VisitType
-        item.RCP_Subject = request.RCP_Subject
-        item.RCP_SubjectName = request.RCP_SubjectName
-        item.RCP_ReceiptDate = request.RCP_ReceiptDate
-        item.RCP_ReceiptTime = request.RCP_ReceiptTime
-        item.RCP_StartTreatTime = request.RCP_StartTreatTime
-        item.RCP_EndTreatTime = request.RCP_EndTreatTime
+        item.RCP_Subject = request.RCP_Subject3EndTreatTime
         item.RCP_Memo = request.RCP_Memo
         item.RCP_IsValid = request.RCP_IsValid
 
@@ -90,21 +87,29 @@ class ReceptionService(BaseService):
 
             setIRC : Insurance = Insurance()
 
-            setIRC.MEM_Idx = user.MEM_Idx
-            setIRC.IRC_Idx = IRCItem.IRC_Idx
-            setIRC.RCP_Idx = RCPItem.RCP_Idx
-            setIRC.PAT_Idx = RCPItem.PAT_Idx
-            setIRC.IRC_Type = IRCItem.IRC_Type
-            setIRC.IRC_CertNum = IRCItem.IRC_CertNum
-            setIRC.IRC_ContractorName = IRCItem.IRC_ContractorName
-            setIRC.IRC_InsuredName = IRCItem.IRC_InsuredName
-            setIRC.IRC_CoName = IRCItem.IRC_CoName
-            setIRC.IRC_EffectiveYYMMDD = IRCItem.IRC_EffectiveYYMMDD
-            setIRC.IRC_ExpiredYYMMDD = IRCItem.IRC_ExpiredYYMMDDD
+            # 비보험일 때 이미 해당 접수의 보험이 있으면 삭제 처리
+            if IRCItem.IRC_Idx > 0 and IRCItem.IRC_Type == "NOR":
+                setIRC.IRC_Idx = IRCItem.IRC_Idx
+                setIRC.IRC_IsValid = False
 
-            retIRC = await self.DbContext.GetItem(eSP.proc_Insurance_SetInsurance, setIRC)
+                await self.DbContext.GetItem(eSP.proc_Insurance_SetInsurance, setIRC)
 
-            if retIRC is None or self.DbContext.retIsSuccess == False:
-                raise ApiException(self.DbContext.retMessage)
+            else:
+                setIRC.MEM_Idx = user.MEM_Idx
+                setIRC.IRC_Idx = IRCItem.IRC_Idx
+                setIRC.RCP_Idx = RCPItem.RCP_Idx
+                setIRC.PAT_Idx = RCPItem.PAT_Idx
+                setIRC.IRC_Type = IRCItem.IRC_Type
+                setIRC.IRC_CertNum = IRCItem.IRC_CertNum
+                setIRC.IRC_ContractorName = IRCItem.IRC_ContractorName
+                setIRC.IRC_InsuredName = IRCItem.IRC_InsuredName
+                setIRC.IRC_CoName = IRCItem.IRC_CoName
+                setIRC.IRC_EffectiveYYMMDD = IRCItem.IRC_EffectiveYYMMDD
+                setIRC.IRC_ExpiredYYMMDD = IRCItem.IRC_ExpiredYYMMDDD
+
+                retIRC = await self.DbContext.GetItem(eSP.proc_Insurance_SetInsurance, setIRC)
+
+                if retIRC is None or self.DbContext.retIsSuccess == False:
+                    raise ApiException(self.DbContext.retMessage)
 
         return DataResponse[Reception_Res].CreateJsonResult(items=ret, message=self.DbContext.retMessage)
