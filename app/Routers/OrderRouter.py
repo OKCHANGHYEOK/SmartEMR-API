addd033023008739136c7a4b97ab55f2412c58fa
OK CHANG HYEOK <dkdldy2@naver.com>
2026-08-20 16:51:28 +0900

    fix : 오타 수정

--- app/Routers/OrderRouter.py
+++ app/Routers/OrderRouter.py
@@ -5,6 +5,6 @@ from app.Schemas.OrderDTO import Order_Req, Order_Res
 from app.Services.Domain import OrderService
 
 class OrderRouter():
-    @router.post("/GetOrder", response_model=DataResponse[Order_Res]):
+    @router.post("/GetOrder", response_model=DataResponse[Order_Res])
     async def GetOrder(request : Order_Req, service : OrderService = Depends(OrderService)):
         return await service.GetOrder(request)
